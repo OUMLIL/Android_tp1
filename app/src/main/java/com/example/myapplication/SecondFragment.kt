7ba9e76b3ddc7d4887c0a3d23c93115f3e9595d5
Oussama ABBOUD <ouabboud@a106imac09.local.isima.fr>
2023-01-16 09:49:26 +0100

    adapter_list_done

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -6,8 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SecondFragment : Fragment() {
+
+    private var recyclerView: RecyclerView? = null
+    private var my_adapter: CustomAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,7 +24,18 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val cameraIntent = Intent("android.media.action.IMAGE_CAPTURE")
-        startActivity(cameraIntent)
+       // val cameraIntent = Intent("android.media.action.IMAGE_CAPTURE")
+       // startActivity(cameraIntent)
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView?.layoutManager = LinearLayoutManager(activity)
+
+        my_adapter = CustomAdapter(arrayOf<String>("str1", "str2", "str3", "str4", "str5"))
+        recyclerView?.adapter = my_adapter
     }
 }
+
+
+// add a recycler view to the secondfragment
+// create the cell: new layout -> cellule
+// creer l'Adapter
